cce68b0986bcfa99000ef513e470a03e870f7c86
Rebah Özkoç <rebahozkoc@hotmail.com>
2021-12-28 00:12:01 +0300

    Create leaving

---
+++ declareleaving.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="parent">
+    <div class="child">
+        <h2> Park a Car</h2>
+        <form action="declareleaving_action.php" method ="POST">
+            <label for="plate_no"> Your Plate No: </label>
+            <input type ="text" id="plate_no" name="plate_no" placeholder="Enter your plate no:"><br>
+            <label for="arrival_date"> Arrival Date: </label>
+            <input type ="date" id="arrival_date" name="arrival_date" placeholder="Enter arrival date"><br> <br>
+            <label for="arrival_time"> Arrival Time: </label>
+            <input type ="time" id="arrival_time" name="arrival_time" placeholder="Enter arrival time"><br> <br>
+            
+            <input type="submit" value = "submit">             
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ declareleaving_action.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SABANCI UNIVERSITY PERSONAL VEHICLE AND PARKING MANAGEMENT SYSTEM </title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+<?php
+include "config.php";
+
+//to-do: 
+
+if(!empty($_POST["plate_no"])){
+    $plate_no = $_POST["plate_no"];
+    $arrival_time = $_POST["arrival_time"];
+    $arrival_date = $_POST["arrival_date"];
+
+    // Return current date from the remote server
+    date_default_timezone_set('Europe/Istanbul');
+    $date = date('y-m-d');
+    $time = date('h:i:s');
+    $plate_no = str_replace(' ', '', $plate_no);
+    $plate_no = strtoupper($plate_no);
+
+    $sql_plate_no = "SELECT cid FROM cars WHERE plate_no = '$plate_no'";
+    
+    $cid = "";
+    $result1= mysqli_query($db, $sql_plate_no);
+    while($row = mysqli_fetch_assoc($result1)){
+        
+        $cid = $row["cid"];
+        echo "Plate no $plate_no";
+        echo "CID $cid";
+        
+    }
+    if ($cid == ""){
+?>
+        <div class="parent" align ="center">
+            <h1><b>OOPS ERROR HAPPENED</b></h1>
+            <p>You entered a wrong plate number.<p>
+            <br>
+            <a href="index.php" class="button button1">MAIN MENU</a>
+        </div>
+<?php
+        return;
+    }
+    $tid = "";
+    $sql_arrival_tid = "SELECT tid FROM parking_date_times WHERE Date = '$arrival_date' and Time = '$arrival_time'";
+    $result23= mysqli_query($db, $sql_arrival_tid);
+    while($row = mysqli_fetch_assoc($result23)){
+        
+        $tid = $row["tid"];
+        echo "Plate no $plate_no";
+        echo "TID $tid";
+        
+    }
+    if ($tid == ""){
+        ?>
+                <div class="parent" align ="center">
+                    <h1><b>OOPS ERROR HAPPENED</b></h1>
+                    <p>You entered a wrong arrival date and time.<p>
+                    <br>
+                    <a href="index.php" class="button button1">MAIN MENU</a>
+                </div>
+        <?php
+                return;
+    }
+
+    $insertleavingdatetimesql = "INSERT INTO leaving_date_times(Date, Time) VALUES ( '$date', '$time')";
+
+    $gettid = "SELECT LAST_INSERT_ID()";
+    $result1 = mysqli_query($db, $insertleavingdatetimesql);
+    $result2 = mysqli_query($db, $gettid);
+
+    while($row = mysqli_fetch_assoc($result2)) {
+        $leave_tid = $row['LAST_INSERT_ID()'];
+        $updatesql = "UPDATE parked_by SET departure_tid = $leave_tid WHERE cid = $cid AND arrival_tid = $tid";
+        $result3 = mysqli_query($db, $updatesql);
+    }
+
+    // Decrease currCapacity
+
+
+    $sql_statement_all= "SELECT P.pname FROM cars C, parked_by B, parking_areas P WHERE C.cid = B.cid and P.pid = B.pid and C.cid = $cid and B.departure_tid = $leave_tid";
+    $result5 = mysqli_query($db, $sql_statement_all);
+    while($row = mysqli_fetch_assoc($result5)) {
+        $pname = $row["pname"];
+        echo "RESULT: " . $pname;
+        ?>
+                <div class="parent" align ="center">
+                    <h1><b>SUCCESS</b></h1>
+                    <p>You are leaving the campus. You can find your car at: <p>
+                        <?php echo $pname ?> 
+                    <br>
+                    <a href="index.php" class="button button1">MAIN MENU</a>
+                </div>
+        <?php
+    }
+}
+else{
+     echo "Plate no must be entered.";
+}
+
+//$sql_statement= SELECT P.pname FROM cars S, parked_by R, parking_area P WHERE S.cid = R.cid and P.pid = R.pid;
+?>
+
+
+</body>
+</html>
